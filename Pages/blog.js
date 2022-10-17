db5ffa79b9715370dfad04405236bd1dcdd338aa
nsblount111300 <nsblount111300@gmail.com>
2022-10-16 23:14:45 -0400

    Initializing Repository

---
+++ Pages/blog.js
@@ -0,0 +1,15 @@
+import blogData from "./blogs.json" assert { type: "json" };
+const blogContainer = document.getElementById("blog-card-container");
+
+blogData.forEach((blog) => {
+  const singleBlog = document.createElement("div");
+  singleBlog.innerHTML = `
+    <div id="js-card">
+    <h4 id="blog-date">${blog.date}</h4>
+    <h3 id="blog-title">${blog.title}</h3>
+    <img id="blog-img" src="${blog.img}">
+    <p id="blog-text">${blog.text}</p>
+    </div>
+  `;
+  blogContainer.appendChild(singleBlog);
+});
