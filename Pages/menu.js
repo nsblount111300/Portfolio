3f26df9dda6e64e4e7164c853860a2496614cc05
nsblount111300 <nsblount111300@gmail.com>
2022-10-17 17:18:01 -0400

    UI Overhaul menu fix + launchpad

---
+++ Pages/menu.js
@@ -0,0 +1,15 @@
+document.addEventListener("click", (e) => {
+  const isDropDownButton = e.target.matches("[data-dropdown-button]");
+  if (!isDropDownButton && e.target.closest("[data-dropdown]") != null) return;
+
+  let currentDropdown;
+  if (isDropDownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]");
+    currentDropdown.classList.toggle("active");
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+    if (dropdown === currentDropdown) return;
+    dropdown.classList.remove("active");
+  });
+});
