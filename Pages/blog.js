177dfce3a3c9e7820e8c7a9816ac18b58a87e98a
Nash Blount <nsblount111300@gmail.com>
2022-10-18 12:18:54 -0400

    Projects page WIP

--- Pages/blog.js
+++ Pages/blog.js
@@ -3,14 +3,14 @@ const blogData = [
     id: "1",
     date: "10/16/2022",
     title: "First JavaScript!",
-    img: "../jsfirstcode.png",
+    img: "../Images/jsfirstcode.png",
     text: "Today I wrote my first lines of JavaScript code! This code specifically allows me to get the information needed for these blog posts!",
   },
   {
     id: "2",
     date: "10/17/2022",
     title: "Drop Down Menu!",
-    img: "../menu.png",
+    img: "../Images/menu.png",
     text: "Today I spent most of my time creating the site's navigation menu! It took me a while and I had to write some new JavaScript that implemented the drop down function.",
   },
 ];
